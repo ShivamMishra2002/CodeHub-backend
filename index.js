5d27ad9b36e5a50b9191e19a5ace6f62fe7e8a5b
Shivam <sm4416683@gmail.com>
2026-01-28 10:15:53 +0530

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const aiRouter = require('./src/routes/aiChatting');
 const videoRouter = require('./src/routes/videoCreator');
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://code-arena-frontend-bay.vercel.app'],
+    origin: ['http://localhost:5173'],
     credentials: true
 }));
 
